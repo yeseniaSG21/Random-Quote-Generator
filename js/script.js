0cee6664053dfabab430d1a11e1d8e0b24591f35
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-09-28 17:19:04 -0700

    Edits to add to my project on the JavaScript file

--- js/script.js
+++ js/script.js
@@ -3,21 +3,54 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+//Array stores my objects each consisting of 4 properties (quote, source, citation, year)
+const quotes = [
+  {
+    quote: "That which does not kill us makes us stronger.",
+    source: "Friedrich Nietzsche",
+    citation: "Twilight of the Idols",
+    year: 1888
+  },
+  {
+    quote: "You must be the change you wish to see in the world.",
+    source: "Mahatma Gandhi",
+    citation: "speech",
+    year: 1913
+  },
+  {
+    quote: "There is some good in this world, and it’s worth fighting for.",
+    source: "J.R.R. Tolkien",
+    citation: "The Lord of the Rings: The Two Towers",
+    year: 1954
+  },
+  {
+    quote: "I am not afraid of storms, for I am learning how to sail my ship.",
+    source: "Louisa May Alcott",
+    citation: "Little Women",
+    year: 1868
+  },
+  {
+    quote: "There is no greater agony than bearing an untold story inside you.",
+    source: "Maya Angelou",
+    citation: "I Know Why the Caged Bird Sings",
+    year: 1969
+  },
+  {
+    quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
+    source: "John Keating",
+    citation: "Dead Poets Society",
+    year: 1989
+  }
+];
 
-/*** 
- * `quotes` array 
-***/
-
-
-
-/***
- * `getRandomQuote` function
-***/
-
-
+/*
+ This function will generate a random number between 0 and length of quotes array.
+ It then will select randomQuote from our array and return the object
+*/
+function getRandomQuote() {
+  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  return randomQuote;
+};
 
 /***
  * `printQuote` function
@@ -29,5 +62,4 @@ project 1 - A Random Quote Generator
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
-
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
