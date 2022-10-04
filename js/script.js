af2029b6aec8494d5cc16f9ef1f1298cadf240ac
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-04 15:01:35 -0700

    Syntax Error Fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /******************************************
 Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+Project 1 - A Random Quote Generator
 ******************************************/
 
 //Array stores my objects each consisting of 4 properties (quote, source, citation, year)
